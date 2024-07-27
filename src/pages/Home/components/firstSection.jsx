9401e44c648b82e847da6b74bc4d87471c643130
Gasbiachraf <gasbi.achraf@gmail.com>
2024-07-27 18:20:11 +0100

    home page Done without carousel

--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -8,30 +8,30 @@ import image3 from "../../../assets/img/Isolation_Mode (1).png"
 import image4 from "../../../assets/img/Isolation_Mode (2).png"
 import image5 from "../../../assets/img/Isolation_Mode (3).png"
 import image6 from "../../../assets/img/Group 53.png"
-import image10 from "../../../assets/img/image 11 (11).png"
+import image7 from "../../../assets/img/banner 1.png"
+import image8 from "../../../assets/img/Banner 2.png"
+import image9 from "../../../assets/img/Rectangle 23.png"
+import image10 from "../../../assets/img/Rectangle 24.png"
+import image11 from "../../../assets/img/Rectangle 25.png"
+import image12 from "../../../assets/img/Rectangle 26.png"
+import image13 from "../../../assets/img/Frame 88.png"
+import image14 from "../../../assets/img/Frame 88 (1).png"
+import image15 from "../../../assets/img/Frame 88 (2).png"
+
+
 import { SlBasket } from "react-icons/sl";
 import { CiHeart } from "react-icons/ci";
 import { CiStar } from "react-icons/ci";
 import { FaStar } from "react-icons/fa";
 export const FirstSectionHome = () => {
     const [product, setProduct] = useContext(MyContext)
-    const x = 8;
 
-    function multiple(params) {
-        return (params)
-        return (params)
-        return (params)
-        return (params)
-        return (params)
-    }
+    const productrate5 = product.filter(element=>(element.rate == 5 && element.category == "sale" ))
+    let pproduct = [...productrate5.slice(0,4)]
 
 
-
-    // const myArray = ['apple', 'banana', 'orange'];
     return (
         <>
-
-
 
             <section className=' bg-center bg-cover bg-no-repeat lg:h-[100vh] lg:w-full relative' style={{ backgroundImage: `url(${image1})` }}>
                 <div className='w-[50vw] absolute end-0 top-24'>
@@ -40,6 +40,7 @@ export const FirstSectionHome = () => {
                     <button className='bg-yellow-400 py-3 px-8 rounded-full'>Shop now</button>
                 </div>
             </section>
+
             <section className='text-center py-10'>
                 <h1 className='text-2xl font-bold '>Find the Perfect Toy </h1>
                 <p className='pb-10'>our collections</p>
@@ -67,7 +68,6 @@ export const FirstSectionHome = () => {
                 </div>
             </section>
 
-
             <section>
                 <h1 className='text-2xl font-bold text-center py-10 '>Top picks for your littleones</h1>
                 <div className='justify-center flex gap-10 text-2xl'>
@@ -82,33 +82,35 @@ export const FirstSectionHome = () => {
 
 
 
-                                <div className=' rounded-lg  lg:w-[22vw]  relative'  >
-                                    <div className='absolute right-3  top-3 text-gray-400 '>
-                                        <div className='flex flex-col gap-2'>
-                                            <CiHeart className='text-3xl' />
-                                            <SlBasket className='text-2xl' />
+                                <div className={element.id > 8 ? "hidden" : ""}>
+                                    <div className=' rounded-lg  lg:w-[22vw]  relative'  >
+                                        <div className='absolute right-3  top-3 text-gray-400 '>
+                                            <div className='flex flex-col gap-2'>
+                                                <CiHeart className='text-3xl' />
+                                                <SlBasket className='text-2xl' />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className='absolute left-2 top-2'>
-                                    <p className={element.category == "sale" ? "block bg-red-400 rounded-full py-1 px-4 text-white font-semibold" : "hidden " }>SALE</p>
+                                        <div className='absolute left-2 top-2'>
+                                            <p className={element.category == "sale" ? "block bg-red-400 rounded-full py-1 px-4 text-white font-semibold" : "hidden "}>SALE</p>
 
-                                    </div>
-                                    <img className='lg:w-[22vw]' src={element.image} alt="" />
-                                    <p className='pt-4 font-medium text-xl'>{element.productName}</p>
-                                    <p className={element.oldprice != 0 ? "text-green-500 text-xl" : "text-black text-xl"}>${element.price}.00 <span className={element.oldprice == 0 ? "hidden" : "text-black line-through text-base"}>${element.oldprice}.00</span></p>
-                                    <div className="">
-                                        <div className='pt-4 flex gap-1'>
-                                            {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
-                                            {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
-                                            {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
-                                            {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
-                                            {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
                                         </div>
+                                        <img className='lg:w-[22vw]' src={element.image} alt="" />
+                                        <p className='pt-4 font-medium text-xl'>{element.productName}</p>
+                                        <p className={element.oldprice != 0 ? "text-green-500 text-xl" : "text-black text-xl"}>${element.price}.00 <span className={element.oldprice == 0 ? "hidden" : "text-black line-through text-base"}>${element.oldprice}.00</span></p>
+                                        <div className="">
+                                            <div className='pt-4 flex gap-1'>
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                            </div>
+                                        </div>
+
+
+
+
                                     </div>
-
-
-
-
                                 </div>
 
 
@@ -124,6 +126,89 @@ export const FirstSectionHome = () => {
 
 
 
+            </section>
+
+            <section >
+                <div className='flex flex-col items-center'> 
+                    <div className='lg:w-[90vw] flex gap-8 py-16'>
+                        <img className='lg:w-100 ' src={image7} alt="" />
+                        <img className='lg:w-100 ' src={image8} alt="" />
+                    </div>
+                    <h1 className='text-2xl font-bold'>Customer loves</h1>
+                    <p>Popular product</p>
+                </div>
+                <div className='flex flex-wrap lg:pl-16 gap-4 py-10'>
+                    {
+                        pproduct.map((element, id) =>
+                            <>
+                                <div>
+                                    <div className=' rounded-lg  lg:w-[22vw]  relative'  >
+                                        <div className='absolute right-3  top-3 text-gray-400 '>
+                                            <div className='flex flex-col gap-2'>
+                                                <CiHeart className='text-3xl' />
+                                                <SlBasket className='text-2xl' />
+                                            </div>
+                                        </div>
+                                        <div className='absolute left-2 top-2'>
+                                            <p className={element.category == "sale" ? "block bg-red-400 rounded-full py-1 px-4 text-white font-semibold" : "hidden "}>SALE</p>
+                                        </div>
+                                        <img className='lg:w-[22vw]' src={element.image} alt="" />
+                                        <p className='pt-4 font-medium text-xl'>{element.productName}</p>
+                                        <p className={element.oldprice != 0 ? "text-green-500 text-xl" : "text-black text-xl"}>${element.price}.00 <span className={element.oldprice == 0 ? "hidden" : "text-black line-through text-base"}>${element.oldprice}.00</span></p>
+                                        <div className="">
+                                            <div className='pt-4 flex gap-1'>
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                                {element.rate == 0 ? <CiStar className='text-yellow-400 text-2xl' /> : <FaStar className='text-yellow-400 text-xl' />}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    }
+                </div>
+            </section>
+            
+            <section>
+
+            </section>
+
+            <section>
+                <h1 className='text-2xl  font-bold text-center'>Recent photoshoots</h1>
+                <p className='text-center py-2'>Check gallery</p>
+                <div className='py-10 flex'>
+                    <img className='lg:w-[25%]' src={image9} alt="" />
+                    <img className='lg:w-[25%]' src={image10} alt="" />
+                    <img className='lg:w-[25%]' src={image11} alt="" />
+                    <img className='lg:w-[25%]' src={image12} alt="" />
+                </div>
+                <div className='flex justify-center gap-6 py-10'>
+                    <div className='rounded-xl lg:w-[28%] lg:h-[30vh] bg-[#F4FAFF] flex flex-col items-center p-4'>
+                        <img src={image13} alt="" />
+                        <p className='text-xl'>24h hour follow up</p>
+                    </div>
+                    <div className='rounded-xl lg:w-[28%] lg:h-[30vh] bg-[#FFFAF4] flex flex-col items-center p-4'>
+                        <img src={image14} alt="" />
+                        <p className='text-xl'>Free shipping for 150$ and up</p>
+                    </div>
+                    <div className='rounded-xl lg:w-[28%] lg:h-[30vh] bg-[#F4FFF5] flex flex-col items-center p-4'>
+                        <img src={image15} alt="" />
+                        <p className='text-xl'>within 7 days</p>
+                    </div>
+                </div>
+                <div className='flex flex-col items-center py-8 '>
+                    <h1 className='text-3xl font-bold pb-2'>Newsletter</h1>
+                    <p className='lg:w-[54vw] text-2xl text-center'>Get 15% off your first purchase! Plus,  be the first to know about sales, new product launches, and exlusive offres!</p>
+                    <div className='py-8'>
+                        <form action="">
+                            <input placeholder='Enter your email' type="text" className='border-2 rounded-xl lg:w-[30vw] py-2 px-4 ' />
+                            <input type="submit" className='bg-blue-600 py-2 px-6 rounded-xl text-white lg:ml-4' value={"Join"} />
+                        </form>
+                    </div>
+                </div>
             </section>
 
 
